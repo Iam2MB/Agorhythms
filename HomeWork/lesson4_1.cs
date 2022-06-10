0c87cb82c3a21e64b983da6a447cdc0069b33f1b
Mirza <mirzano05@gmail.com>
2022-06-10 17:51:51 +0300

    DZ_4

---
+++ HomeWork/lesson4_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HomeWork
+{
+    internal class lesson4_1
+    {
+        public static void StringSearchTest()
+        {
+            Random random = new Random();
+            HashSet<int> hashset = new HashSet<int>();
+            int[] array = new int[100000];
+            int stringsCount = array.Length;
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                int k = random.Next(1, stringsCount);
+                hashset.Add(k);
+                array[i] = k;
+            }
+
+            Console.Write($"Введите значение до {stringsCount}: ");
+            int number = int.Parse(Console.ReadLine());
+            Stopwatch timeSpent = new Stopwatch();
+
+            timeSpent.Start();
+            int temp = 0;
+            foreach (var item in array)
+            {
+                if (item.Equals(number))
+                {
+                    temp++;
+                    break;
+                }
+            }
+            if (temp == 0)
+            {
+                Console.WriteLine("\nТакого значения нет в array");
+            }
+            timeSpent.Stop();
+            Console.WriteLine($"Time Spent of Array:\t{timeSpent.Elapsed}");
+
+            timeSpent.Restart();
+            temp = 0;
+            foreach (var item in hashset)
+            {
+                if (item.Equals(number))
+                {
+                    break;
+                }
+            }
+            if (temp == 0)
+            {
+                Console.WriteLine("\nТакого значения нет в hashset");
+            }
+            timeSpent.Stop();
+            Console.Write($"Time spent of HashSet:\t{timeSpent.Elapsed}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
